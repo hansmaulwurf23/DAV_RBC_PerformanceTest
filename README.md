c8b45baf6495169d34b60b55b814d8f9caa10e2e
GrafRudi <rudi.grave@gmx.de>
2022-02-20 13:52:24 +0100

    added calls and meassurements

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DAV_RBC_PerformanceTest
+Usage
+--- python main.py
+It might be that you need to install requests via
+--- python -m pip install requests
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import webbrowser
+import requests
+
+rbc_url = 'https://www.dav-rbc.de/072/185829.php'
+MAX_OPEN_WEBSITE = 5
+
+# webbrowser.open(rbc_url)
+i=0
+
+while i <= 10:
+    #Open Website in default browser MAX_OPEN_WEBSITE times
+    j=1
+    while j <= MAX_OPEN_WEBSITE:
+        webbrowser.open(rbc_url)
+        j=j+1
+    #Send GET request and print time
+    response = requests.get(rbc_url).elapsed.total_seconds()
+    i=i+1
+    print(response)
+
+print ("Danke")
